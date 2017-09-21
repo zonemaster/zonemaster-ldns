8d7857032b4fbce85cee7f145dea71a552aafea7
Mattias Päivärinta <mattias.paivarinta@iis.se>
2017-09-21 11:10:19 +0200

    Revert "Document the new OPT RR methods."
    This reverts commit bec60ca4c2bc16f7bea27bd2921d21d49d0570d6.

--- lib/Net/LDNS/RR/OPT.pm
+++ lib/Net/LDNS/RR/OPT.pm
@@ -19,18 +19,6 @@ Class representing the special OPT pseudo-RR used for EDNS0. See RFC 2671.
 
 Get the UDP data size value.
 
-=item extended_rcode()
-
-Get the extended RCODE field. Note that this is only the upper eight bits of the full 12-bit extended RCODE. 
-
-=item edns_version()
-
-Get the EDNS version number. If this is not zero, something weird is going on.
-
-=item edns_z()
-
-The 16-bit flag field. The only defined flag as of this writing is the DNSSEC DO flag in the topmost bit.
-
 =back
 
 =cut
