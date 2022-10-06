6561ea435ca8499296889a928fbb66e156b4eeb6
Alexandre Pion <pion@afnic.fr>
2022-10-06 12:25:01 +0200

    Derive CDS (CDNSKEY) from DS (DNSKEY)

--- lib/Zonemaster/LDNS/RR/CDNSKEY.pm
+++ lib/Zonemaster/LDNS/RR/CDNSKEY.pm
@@ -3,7 +3,7 @@ package Zonemaster::LDNS::RR::CDNSKEY;
 use strict;
 use warnings;
 
-use parent 'Zonemaster::LDNS::RR';
+use parent 'Zonemaster::LDNS::RR::DNSKEY';
 
 1;
 
@@ -13,7 +13,7 @@ Zonemaster::LDNS::RR::CDNSKEY - Type CDNSKEY record
 
 =head1 DESCRIPTION
 
-A subclass of L<Zonemaster::LDNS::RR>, so it has all the methods of that class available in addition to the ones documented here.
+A subclass of L<Zonemaster::LDNS::RR::DNSKEY>, so it has all the methods of that class available in addition to the ones documented here.
 
 =head1 METHODS
 
